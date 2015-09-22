8653271d9eba373b8b83323b95c3711790401765
micovo <mica@micovo.cz>
2015-09-22 19:56:10 +0200

    TME CZ import procedures completed

--- src/TidyStorage/Suppliers/TMESupplier.cs
+++ src/TidyStorage/Suppliers/TMESupplier.cs
@@ -22,13 +22,20 @@ namespace TidyStorage.Suppliers
 {
     class TMESupplier : Supplier
     {
+        SupplierPart part;
+
+        public override string Name { get { return "TME CZ"; } }
+
         public TMESupplier(string part_number) : base(part_number)
         {
 
         }
 
-        public override string Name { get { return "TME CZ"; } }
-
+        
+        /// <summary>
+        /// /
+        /// </summary>
+        /// <returns></returns>
         public override string GetLink()
         {
             return "http://www.tme.eu/cz/details/" + part_number + "/";
@@ -36,11 +43,14 @@ namespace TidyStorage.Suppliers
 
         public override SupplierPart DownloadPart()
         {
-            SupplierPart p = new SupplierPart();
+            HtmlDocument pricesDocument = null;
+            HtmlDocument mainDocument = null;
 
-            p.order_num = part_number;
+            part = new SupplierPart();
+            part.order_num = part_number;
 
-            string refferer = "http://www.tme.eu/cz/details/"+ part_number + "/";
+
+            string refferer = "http://www.tme.eu/cz/details/"+ HttpUtility.HtmlEncode(part_number.Trim()) + "/";
             string error = "";
             string responce = "";
             string origin = "http://www.tme.eu";
@@ -48,6 +58,7 @@ namespace TidyStorage.Suppliers
             string url = refferer;
             CookieContainer cc = new CookieContainer();
 
+            //Download part page and get cookies form price API
             using (HttpWebResponse resp = WebClient.Request(url, out error, cc))
             {
                 if (resp != null)
@@ -55,55 +66,203 @@ namespace TidyStorage.Suppliers
                     StreamReader reader = new StreamReader(resp.GetResponseStream());
 
                     responce = reader.ReadToEnd();
+                    mainDocument = new HtmlDocument();
+                    mainDocument.LoadHtml(responce);
                 }
             }
 
 
+            //Download prices from the price API
             url = "http://www.tme.eu/cz/_ajax/ProductInformationPage/_getStocks.html";
 
             NameValueCollection nvc = new NameValueCollection();
-
             nvc["symbol"] = part_number;
 
-            using (HttpWebResponse resp = WebClient.Request(url, out error, cc,nvc, host, refferer, origin))
+            using (HttpWebResponse resp = WebClient.Request(url, out error, cc, nvc, host, refferer, origin))
             {
                 if (resp != null)
                 {
                     StreamReader reader = new StreamReader(resp.GetResponseStream());
 
-                    responce = reader.ReadToEnd();
+                    string prices_responce = reader.ReadToEnd();
 
-                    if (responce != "")
+                    if (prices_responce != "")
                     {
-                        JToken jt = JToken.Parse(responce);
-                        JArray ja = (JArray)jt["Products"][0]["Prices"];
+                        JToken jt = JToken.Parse(prices_responce);
+                        string priceTableStr = (string)jt["Products"][0]["PriceTpl"];
 
-                        if (ja != null)
-                        {
-                            p.prices = new List<PartPrice>();
+                        priceTableStr = "<html><head></head><body>" + priceTableStr + "</body></html>";
 
-                            foreach (JToken jat in ja)
-                            {
-
-                                int min = (int)jat["Amount"];
-                                string temp = (string)jat["Price"];
-                                temp = temp.Replace(" Kč","").Replace('.',',');
-                                float price = float.Parse(temp);
-
-                                p.prices.Add(new PartPrice(min, price));
-                            }
-
-                        }
-
+                        pricesDocument = new HtmlDocument();
+                        pricesDocument.LoadHtml(priceTableStr);
                     }
                 }
             }
 
+
+            if (pricesDocument != null)
+            {
+                GetPrices(pricesDocument);
+            }
+
+            if (mainDocument != null)
+            {
+                GetProductDescriptors(mainDocument);
+            }
+
+            return part;
+        }
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="document"></param>
+        private void GetProductDescriptors(HtmlDocument document)
+        {
+            part.rows = new List<PartRow>();
+            HtmlNodeCollection hnc = document.DocumentNode.SelectNodes("//div[contains(@class,'symbol-box')]");
+
+            if (hnc != null)
+            {
+                var symbol_nodes = hnc.First().ChildNodes.First(x => (x.Name == "table"));
+
+                if (symbol_nodes != null)
+                {
+                    HtmlNode[] rows = symbol_nodes.ChildNodes.Where(x => (x.Name == "tr")).ToArray();
+
+                    foreach (HtmlNode tr_node in rows)
+                    {
+                        HtmlNode[] childs = tr_node.ChildNodes.Where(x => (x.Name == "td")).ToArray();
+
+                        if (childs.Length == 2)
+                        {
+                            string name = childs[0].InnerText.Trim().Trim(':');
+                            string value = childs[1].InnerText.Trim();
+
+
+                            part.rows.Add(new PartRow(name, value));
+                        }
+                    }
+                }
+            }
+
+
+            hnc = document.DocumentNode.SelectNodes("//a[@class='pdf']");
+
+            if (hnc != null)
+            {
+                var datasheet_a_node = hnc.First();
+                if (datasheet_a_node != null)
+                {
+                    string name = "Datasheet";
+                    string value = "http://www.tme.eu" + datasheet_a_node.Attributes["href"].Value;
+                    part.rows.Add(new PartRow(name, value));
+                }
+            }
+
            
+            hnc = document.DocumentNode.SelectNodes("//div[@id='specification']");
 
-            return p;
+            if (hnc != null)
+            {
+                HtmlNode hn = hnc.First().ChildNodes.First(x => x.Name == "table");
+
+                HtmlNode[] rows = hn.ChildNodes.Where(x => (x.Name == "tr")).ToArray();
+
+                foreach (HtmlNode tr_node in rows)
+                {
+                    HtmlNode[] childs = tr_node.ChildNodes.Where(x => (x.Name == "td")).ToArray();
+
+                    if (childs.Count() == 3)
+                    {
+                        string value = childs[1].InnerText;
+                        value = value.Replace('µ', 'u');
+                        value = value.Replace('±', ' ');
+                        value = value.Replace("Ω", "ohm");
+                        value = value.Trim();
+
+                        //Temperature split
+                        if ((value.Contains("...") && (value.EndsWith("°C"))))
+                        {
+                            var split_values = value.Split(new string[] { "..." }, StringSplitOptions.None);
+                            if (split_values.Count() > 1)
+                            {
+                                part.rows.Add(new PartRow(childs[0].InnerText.Trim() + " MIN", split_values[0] + "°C"));
+                                part.rows.Add(new PartRow(childs[0].InnerText.Trim() + " MAX", split_values[1]));
+                                continue;
+                            }
+                        }
+
+                        part.rows.Add(new PartRow(childs[0].InnerText.Trim(), value));
+                    }
+                }
+            }
+        }
 
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="document"></param>
+        private void GetPrices(HtmlDocument document)
+        {
+            try
+            {
+                HtmlNodeCollection hnc = document.DocumentNode.SelectNodes("//tbody[@id='prices_body']");
+
+                if (hnc != null)
+                {
+                    part.prices = new List<PartPrice>();
+
+                    HtmlNode hn = hnc.First();
+
+                    foreach (HtmlNode xx in hn.ChildNodes)
+                    {
+                        if ((xx.Name == "tr") && (xx.ChildNodes.Count > 0) && (xx.ChildNodes[1].Name == "td"))
+                        {
+                            HtmlNode[] tds = xx.ChildNodes.Where(x => x.Name == "td").ToArray();
+
+                            if (tds.Length == 2)
+                            {
+                                string tda = HttpUtility.HtmlDecode(tds[0].InnerText).Trim();
+                                string tdp = HttpUtility.HtmlDecode(tds[1].InnerText).Trim().Replace(".", ",");
+
+                                if (tdp.Contains("Kč"))
+                                {
+                                    part.currency = "CZK";
+
+                                    var aaa = tda.Split('-');
+                                    var min = int.Parse(aaa[0].Trim().Trim('+'));
+                                    var max = int.MaxValue;
+
+                                    if (aaa.Length > 1)
+                                    {
+                                        max = int.Parse(aaa[1].Trim());
+                                    }
+
+
+                                    int ix = tdp.IndexOfAny(("0123456789").ToCharArray());
+                                    var price = float.Parse(tdp.Substring(ix, tdp.Length - ix - 3));
+
+                                    part.prices.Add(new PartPrice(min, max, price));
+                                }
+                            }
+                        }
+                    }
+
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("TME web importer failed. " + ex.Message);
+            }
+
+            //Fix prices maximal amount
+            for (int i = 0; i < part.prices.Count - 1; i++)
+            {
+                if (part.prices[i].amount_max > part.prices[i + 1].amount_min) part.prices[i].amount_max = part.prices[i + 1].amount_min - 1;
+            }
         }
     }
 }
